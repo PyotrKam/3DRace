fdde07bfcb89a7d44ddbb72ea33a55e6ae8f7b3b
Pyotr <pyotrk@inbox.lv>
2025-06-30 22:41:44 +0300

    Menu_2

---
+++ Assets/Scripts/UI/Buttons/Base/UISelectableButton.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UISelectableButton : UIButton
+{
+    [SerializeField] private Image selectImage;
+
+    public UnityEvent OnSelect;
+    public UnityEvent OnUnSelect;
+
+    public override void SetFocuse()
+    {
+        base.SetFocuse();
+
+        selectImage.enabled = true;
+
+        OnSelect?.Invoke();
+    }
+
+    public override void SetUnFocuse()
+    {
+        base.SetUnFocuse();
+
+        selectImage.enabled = false;
+
+        OnUnSelect?.Invoke();
+    }
+}
