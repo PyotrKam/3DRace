5a78060177057a93a09cb9028454eda74bc4743d
Pyotr <pyotrk@inbox.lv>
2025-07-01 22:32:56 +0300

    Added Music
    Added main menu music

---
+++ Assets/Scripts/UI/Buttons/Base/UIButtonSound.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class UIButtonSound : MonoBehaviour
+{    
+    [SerializeField] private AudioClip hover;
+    [SerializeField] private AudioClip click;
+
+    private new AudioSource audio;
+
+    private UIButton[] uIButtons;
+
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+
+        uIButtons = GetComponentsInChildren<UIButton>(true);
+
+        for (int i = 0; i < uIButtons.Length; i++)
+        {
+            uIButtons[i].PointerEnter += OnPointerEnter;
+            uIButtons[i].PointerClick += OnPointerClicked;
+        }
+    }  
+
+    private void OnDestroy()
+    {
+        for (int i = 0; i < uIButtons.Length; i++)
+        {
+            uIButtons[i].PointerEnter -= OnPointerEnter;
+            uIButtons[i].PointerClick -= OnPointerClicked;
+        }
+    }
+
+    private void OnPointerEnter(UIButton arg0)
+    {
+        audio.PlayOneShot(hover);
+    }
+
+    private void OnPointerClicked(UIButton arg0)
+    {
+        audio.PlayOneShot(click);
+    }
+
+    
+}
