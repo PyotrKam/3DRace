fecb0c765d515d88426460f5e0e0927eb7a8d2dd
Pyotr <pyotrk@inbox.lv>
2025-07-02 23:19:39 +0300

    Resolution Settings

---
+++ Assets/Scripts/Settings/ResolutionSetting.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu]
+public class ResolutionSetting : Setting
+{
+    [SerializeField]
+    private Vector2Int[] avalibaleResolutions = new Vector2Int[]
+    {
+        new Vector2Int(800, 600),
+        new Vector2Int(1280, 720),
+        new Vector2Int(1600, 900),
+        new Vector2Int(1920, 1080),
+     };
+
+    private int currentResolutionIndex = 0;
+
+    public override bool isMinValue {get => currentResolutionIndex == 0;}
+    public override bool isMaxValue {get => currentResolutionIndex == avalibaleResolutions.Length - 1; }
+
+    public override void SetNextValue()
+    {
+        if (isMaxValue == false)
+        {
+            currentResolutionIndex++;
+        }
+    }
+
+    public override void SePreviousValue()
+    {
+        if (isMinValue == false)
+        {
+            currentResolutionIndex--;
+        }
+    }
+
+    public override object GetValue()
+    {
+        return avalibaleResolutions[currentResolutionIndex];
+    }
+
+    public override string GetStringValue()
+    {
+        return avalibaleResolutions[currentResolutionIndex].x + "x" + avalibaleResolutions[currentResolutionIndex].y;
+    }
+
+    public override void Apply()
+    {
+        Screen.SetResolution(avalibaleResolutions[currentResolutionIndex].x, avalibaleResolutions[currentResolutionIndex].y, true);
+    }
+}
